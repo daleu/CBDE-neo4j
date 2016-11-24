3a1eb2ac1d8c87c2ae98aaf8fc340d451fa27ccb
Daniel Villanueva <danielvillanueva94@gmail.com>
2016-11-24 14:26:21 +0100

    Initial

---
+++ neo.py
@@ -0,0 +1,13 @@
+from neo4j.v1 import GraphDatabase, basic_auth
+
+
+driver = GraphDatabase.driver("bolt://localhost", auth=basic_auth("neo4j", "abcd"))
+session = driver.session()
+
+session.run("CREATE (a:Person {name:'Arthur', title:'King'})")
+
+result = session.run("MATCH (a:Person) WHERE a.name = 'Arthur' RETURN a.name AS name, a.title AS title")
+for record in result:
+    print("%s %s" % (record["title"], record["name"]))
+
+session.close()
